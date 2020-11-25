966144ab1b4cbbc79a67f846c20aff54a1cb1237
Rohmat Tri Indra <rohmat.trie@gmail.com>
2020-11-25 17:00:34 +0700

    Refactor script with argumen parser

--- aruba_controller/client_utilization.py
+++ aruba_controller/client_utilization.py
@@ -1,18 +1,24 @@
 from netmiko import ConnectHandler
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily, Gauge
 from prometheus_client import start_http_server
-import json, os, time
+import json, os, time, sys
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--source-file", dest="list_ap", help="List of Access Point Aruba")
+    parser.add_argument("-o", "--output-client", dest="output_client", help="Output Client Usage each Access Point")
+    parser.add_argument("-j", "--output-json", dest="output_client_json", help="Output Client Usage each AP with JSON Format" )
+    parser.add_argument("-p", "--port", dest="port", help="listening port service" )
+    parser.add_argument("-ip", "--ip", dest="ip", help="IP Address Controller" )
+
+    return parser.parse_args()
 
 def gather_clients():
+    args = get_args()
+    ssh_connection = ConnectHandler(device_type="aruba_os", ip=args.ip, username="controller_user", password="controller_pass", port=22)
 
-    ssh_connection = ConnectHandler(
-        device_type="aruba_os",
-        ip="192.168.1.10",
-        username="admin",
-        password="password",
-        port=22
-    )
-    with open("list_ap") as f:
+    with open(args.list_ap) as f:
         devices_list = f.read().splitlines()
 
     for device in devices_list:
@@ -20,15 +26,12 @@ def gather_clients():
         result = AP[15:17].replace('/','')
         outfile = "ap_name:" + device + " | " + "value:" + result + "\n"
         print (outfile)
-        hasil = open ("list_clients_ap", "a")
-        hasil.write(outfile)
-        hasil.close()
-
+        result = open (args.output_client, "a")
+        result.write(outfile)
+        result.close()
+    ssh_connection.disconnect()
 
 def line_to_dict(split_Line):
-    # Assumes that the first ':' in a line
-    # is always the key:value separator
-
     line_dict = {}
     for part in split_Line:
         key, value = part.split(":", maxsplit=1)
@@ -37,25 +40,20 @@ def line_to_dict(split_Line):
     return line_dict
 
 def convert_json():
-    f = open("list_clients_ap", "r")
+    f = open(args.output_client, "r")
     content = f.read()
     splitcontent = content.splitlines()
-
-    # Split each line by pipe
     lines = [line.split(' | ') for line in splitcontent]
-
-    # Convert each line to dict
     lines = [line_to_dict(l) for l in lines]
-
-    # Convert to json file
-    with open("clients.json", 'w') as fout:
+    with open(args.output_client_json, 'w') as fout:
         json.dump(lines, fout, indent=4)
 
 class CustomCollector(object):
     def __init__(self):
         pass#
     def collect(self):
-        prome_json =  open("clients.json",)
+        args = get_args()
+        prome_json =  open(args.output_client_json)
         data = json.load(prome_json)
 
         for key in data:
@@ -65,11 +63,16 @@ class CustomCollector(object):
 
 
 if __name__ == "__main__":
-    print ("the apps is running on port 9001")
-    start_http_server(9001)
+    args = get_args()
+    start_http_server(int(args.port))
+    print ("The aruba json exporter running on port " + str(args.port) )
+    #The first running to generate file
+    gather_clients()
+    convert_json()
     REGISTRY.register(CustomCollector())
-    while True:        
+    while True:
+        #Continue to generate file each 120 seconds 
         gather_clients()
         convert_json()
-        remove_temp = os.system("rm -rf list_clients_ap")
+        remove_temp = os.system("rm -rf " + args.output_client )
         time.sleep(120)
